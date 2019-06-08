89cb4225db36e5cae7e0be053447d8829757944a
Mohammed slimani <slimani.med.21@gmail.com>
2019-06-08 18:28:52 +0100

    HeaderParser : Ez PZ

--- Header parser/server.js
+++ Header parser/server.js
@@ -5,4 +5,10 @@ const PORT = 3000;
 const app  = express();
 app.use(cors({ optionSuccessStatus : 200}));
 
+app.use('/api/whoami',(req,res)=>{
+    let {ip,headers:{"accept-language":language,"user-agent":software}} = req;
+    console.log(ip, software, language);
+    res.json({ip,software,language});
+})
+
 app.listen(PORT,()=>console.log(`The server is running on port ${PORT}`));
