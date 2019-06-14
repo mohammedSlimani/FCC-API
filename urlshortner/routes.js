eefda2df7f340b697ac3685b615b7fbbc744f2be
Mohammed slimani <slimani.med.21@gmail.com>
2019-06-14 21:45:57 +0100

    Parsed the url to get a better result

--- urlshortner/routes.js
+++ urlshortner/routes.js
@@ -1,63 +1,60 @@
 const express = require("express");
-const Router = express.Router();
-const dns = require('dns');
-const db = require('./db')
+const Router  = express.Router();
+const dns     = require('dns');
+const db = require('./db');
+const urlParser = require('url');
 
 const DOMAIN = 'https://smed-url.glitch.me/api/shorturl/'
 
-Router.post('/new', (req, res, next) => {
-    let { body: { url } } = req;
-    //treat the url cz the dns doesn't care about the protocol
-    url = url.split('//').pop();
-    console.log("processing ", url)
-    dns.lookup(url, async (err, adress, family) => {
-        try {
-            if (err) res.json({ "error": "invalid URL" })
-            else {
-                let existing = await db.getUrlByUrl(url);
-                //console.log("existing url fetched :",existing);
-                if (existing.length) {
-                    console.log("url exist ...");
-                    const foundUrl = existing[0];
-                    res.json({
-                        'origina_url': foundUrl.url,
-                        'short_url': DOMAIN + foundUrl.num
-                    })
-                } else {
-                    console.log("creating a new url ...")
-                    let newone = await db.createAndSaveUrl(url);
-                    res.json({
-                        'origina_url': newone.url,
-                        'short_url': DOMAIN + newone.num
-                    })
-                }
-            }
-        } catch (err) {
-            console.log("err ", err);
+Router.post('/new',(req,res,next)=>{
+  let {body:{url}} = req;
+  //treat the url cz the dns doesn't care about the protocol
+  url = urlParser.parse(url);
+  console.log("processing ",url)
+  dns.lookup(url.host,async(err,adress,family)=>{
+    try{
+        if(err) res.json({"error":"invalid URL"})   
+      else{
+        let existing = await db.getUrlByUrl(url.href);
+        //console.log("existing url fetched :",existing);
+        if(existing.length){
+          console.log("url exist ...");
+          const foundUrl = existing[0];
+          res.json({'origina_url':foundUrl.url,
+                   'short_url':DOMAIN + foundUrl.num})
+        }else{
+          console.log("creating a new url ...")
+          let newone = await db.createAndSaveUrl(url.href);
+          res.json({'origina_url':newone.url,
+                   'short_url':DOMAIN + newone.num})
         }
-
-    })
+      }
+    }catch(err){
+      console.log("err ",err);
+    }
+    
+  })
 })
 
-Router.get('/:shorturl', (req, res, next) => {
-    let shorturl = req.params.shorturl;
-    if (isNaN(shorturl)) res.json({ "Not found": "no url found, add it here: https://smed-url.glitch.me " })
-    try {
-        const existingUrl = db.getUrlByNum(shorturl, (err, founded) => {
-            if (err) next(err)
-            else if (founded.length) {
-                console.log('after the query I find', founded);
-                console.log("redirecting to:", founded[0].url);
-                res.redirect('https://' + founded[0].url);
-                res.end();
-            } else {
-                res.json({ "Not found": "no url found, add it here: https://smed-url.glitch.me " })
-            }
-        });
-    } catch (err) {
-        console.log(err);
-        res.json({ "error": "Catched Error" });
-    }
+Router.get('/:shorturl',(req,res,next)=>{
+  let shorturl = req.params.shorturl;
+  if(isNaN(shorturl)) res.json({"Not found":"no url found, add it here: https://smed-url.glitch.me "})
+  try{
+    const existingUrl = db.getUrlByNum(shorturl,(err,founded)=>{
+      if(err) next(err)
+      else if(founded.length){
+        console.log('after the query I find',founded);
+        console.log("redirecting to:",founded[0].url);
+        res.redirect('https://'+founded[0].url);
+        res.end();
+      }else{
+        res.json({"Not found":"no url found, add it here: https://smed-url.glitch.me "})
+      }
+    });
+  }catch(err){
+    console.log(err);
+    res.json({"error":"Catched Error"});
+  } 
 })
 
 module.exports = Router;
